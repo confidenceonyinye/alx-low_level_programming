94693e17b4435d3c9e1d91f4b4642cb2bfea56ae
confidenceonyinye <treasureonyinyechi185@gmail.com>
2022-09-08 19:47:12 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *main - Entry point
+ *
+ *   *Return: Always 0 (Success)
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	        return (0);
+
+}
+
+
+
+Show less
+
+<https://github.com/AlagaIbile22/alx-low_level_programming|AlagaIbile22/alx-low_level_programming>AlagaIbile22/alx-low_level_programming | Added by GitHub
+
+03:38
+
+https://github.com/AlagaIbile22/alx-low_level_programming/blob/master/0x00-hello_world/100-intel
+
+100-intel
+
+#!/bin/bash
+
+gcc -S -masm=intel $CFILE
+
+
+
+<https://github.com/AlagaIbile22/alx-low_level_programming|AlagaIbile22/alx-low_level_programming>AlagaIbile22/alx-low_level_programming | Added by GitHub
+
+03:38
+
+https://github.com/AlagaIbile22/alx-low_level_programming/blob/master/0x00-hello_world/101-quote.c
+
+101-quote.c
+
+#include <stdio.h>
+
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  *main - Entry point
