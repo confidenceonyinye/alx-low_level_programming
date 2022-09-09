e57c5f9c04dcc0e6a30cf22d82bbb580a637ee9b
confidenceonyinye <treasureonyinyechi185@gmail.com>
2022-09-08 19:52:27 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -21,39 +21,3 @@ int main(void)
 	        return (0);
 
 }
-
-
-
-Show less
-
-<https://github.com/AlagaIbile22/alx-low_level_programming|AlagaIbile22/alx-low_level_programming>AlagaIbile22/alx-low_level_programming | Added by GitHub
-
-03:38
-
-https://github.com/AlagaIbile22/alx-low_level_programming/blob/master/0x00-hello_world/100-intel
-
-100-intel
-
-#!/bin/bash
-
-gcc -S -masm=intel $CFILE
-
-
-
-<https://github.com/AlagaIbile22/alx-low_level_programming|AlagaIbile22/alx-low_level_programming>AlagaIbile22/alx-low_level_programming | Added by GitHub
-
-03:38
-
-https://github.com/AlagaIbile22/alx-low_level_programming/blob/master/0x00-hello_world/101-quote.c
-
-101-quote.c
-
-#include <stdio.h>
-
-#include <unistd.h>
-
-
-
-/**
- *
- *  *main - Entry point
