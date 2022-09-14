58d93ce28d84341f7af5397a82104083bd12d1cd
confidenceonyinye <treasureonyinyechi185@gmail.com>
2022-09-13 19:55:33 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - entry point
- *
- * Description: Prints the alphabet with _putchar ten times
- *
- * Return: void
- */
-void print_alphabet_x10(void) 
-{  
-char c;  
+* print_alphabet_x10 - entry point
+*
+* Description: Prints the alphabet with _putchar ten times
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+char c;
 int i;
-for (i = 0; i < 10; i++)    
-{      
-for (c = 'a'; c <= 'z'; c++)	
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-}      
-_putchar('\n');      
-} 
+}
+ _putchar('\n');
 }
