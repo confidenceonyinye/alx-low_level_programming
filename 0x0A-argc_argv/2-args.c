2df9e23de66ff7dd0fada18e530f8b7bb9806ae1
confidenceonyinye <treasureonyinyechi185@gmail.com>
2022-09-30 21:43:41 -0700

    just an invisible being

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,6 @@
 * main - Print the name of the program
 * @argc: Count arguments
 * @argv: Arguments
-*
 * Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
@@ -15,7 +14,7 @@ if (argc > 0)
 {
 /*WHILE - Print each arguments*/
 while (count < argc)
-{	  
+{
 printf("%s\n", argv[count]);
 count++;
 }
