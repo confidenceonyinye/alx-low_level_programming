c9bac2eb16e6ece5a870f830c0f6e647cc98c633
confidenceonyinye <treasureonyinyechi185@gmail.com>
2022-10-05 20:37:44 -0700

    Save

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -5,7 +5,7 @@
 * Return: On success 1.
 * On error, -1 is returned, and error is set approximately
 */
-int _putchar(char c) 
-{ 
-return(write(1, &c, 1)); 
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
